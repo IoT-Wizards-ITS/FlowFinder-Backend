8d37e09a56b86c857ff3082faa3afc7ae7bad100
Ardy <ardyprasyah@gmail.com>
2024-05-12 15:16:25 +0700

    no user

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,6 @@
-const { registerUserHandler, loginUserHandler } = require('./userHandler');
 const { gsmDataHandler } = require('./gsmHandler');
 
 const routes = [
-    {
-        method: 'POST',
-        path: '/register',
-        handler: registerUserHandler,
-    },
-    {
-        method: 'POST',
-        path: '/login',
-        handler: loginUserHandler,
-    },
     {
         method: 'POST',
         path: '/gsm-data',
