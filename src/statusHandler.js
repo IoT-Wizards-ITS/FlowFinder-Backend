428098df5e5d489941facefafac8f40b716eef4f
Synmary <ardyprasyah@gmail.com>
2024-06-01 08:50:19 +0000

    a

--- src/statusHandler.js
+++ src/statusHandler.js
@@ -1,9 +1,12 @@
+const { getLatestStatusData } = require("./db/getData");
+
+const latestStatus = getLatestStatusData() 
 const statusHandler = (req, res) => {
     res.status(200).json({
         status: 'success',
         message: 'Status data successfully displayed',
         data: {
-            handlerStatus,
+            latestStatus,
         },
     });
 };
